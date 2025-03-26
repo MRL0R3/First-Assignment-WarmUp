3bd20d13db43e91c8718462283a2f696e07cf182
MRL0R3 <ahmadrezajafary1384@gmail.com>
2025-03-27 01:15:53 +0330

    warm up done

--- AP1403 - WarmUp/src/main/java/Exercises.java
+++ AP1403 - WarmUp/src/main/java/Exercises.java
@@ -6,10 +6,12 @@ public class Exercises {
     public static boolean isPrime(long n) {
         if (n <= 1) {return false;}
         if (n == 2) {return true;}
-        for(int i = 2 ; i <Math.sqrt(n) ; i++){
+        for(int i = 2 ; i <Math.sqrt(n) ; i++)
+        {
             if (n % i == 0)
                 return false;}
-
+        if(n>0)
+            return true;
         return false;
     }
 
